d69470cb38d6c14e29f5965a730159eaa646e5db
harryge00 <harryge00@gmail.com>
2016-07-11 22:45:12 +0800

    leetcode 207 working

--- lc207.cc
+++ lc207.cc
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
-        
+        vector<vector<int>> nextCourses(numCourses, vector<int>(0));
+        for()
     }
 };
---
+++ vector.cc
@@ -0,0 +1,10 @@
+int main() {
+	vector<vector<int>> A(dimension, vector<int>(dimension));
+	vector<vector<int>> myvect = { {10,20,30,40},
+                               {50,60,70,80} };
+
+	vector<vector<int>> myvect{ vector<int>{10,20,30,40},
+                            vector<int>{50,60,70,80} };
+
+
+}
