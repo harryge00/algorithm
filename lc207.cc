908fdda159485c63ec031ed074c32885fced9637
harryge00 <harryge00@gmail.com>
2016-07-11 23:56:50 +0800

    lc153 ac

--- lc207.cc
+++ lc207.cc
@@ -1,7 +1,17 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
-        vector<vector<int>> nextCourses(numCourses, vector<int>(0));
+        vector<unordered_set<int>> graph;
+        vector<int> indegree(numCourses, 0);
+        for(int i = 0; i < numCourses; i++) {
+        	unordered_set<int> set;
+        	graph.push_back(set);
+        }
+        for(vector<pair<int, int>>::iterator it = prerequisites.begin(); it != prerequisites.end(); it++) {
+        	graph[it->first].insert(it->second);
+        	indegree[it->first]++;
+        }
         for()
+        for(int i = 0; i < )
     }
 };
