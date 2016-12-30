848f844fadc61fc94a3d5d373103b6c0a376adf0
harryge00 <harryge00@gmail.com>
2016-12-31 00:22:56 +0800

    class_tester

--- class_tester.cc
+++ class_tester.cc
@@ -8,30 +8,29 @@ private:
 	int min;
 	bool found;
 public:
-	void helper(vector<int>& coins, int cur, int count, int remain) {
-		if(remain < 0) return;
-		if(coins[cur] == remain) {
+    bool helper(vector<int>& coins, int cur, int count, int remain) {
+		cout<<coins[cur]<<"cur:"<<cur<<","<<count<<","<<remain<<endl;
+		if(remain < 0) return false;
+		if(remain == 0) {
 			if(count < min) {
 				min = count;
-				found = true;
 			}
-			return;
-		} else if (coins[cur] > remain) {
-			while(cur >= 0 && coins[cur] > remain) {
-				cur--;
-			}
+	        return true;
 		}
 		while(cur>=0) {
-			helper(coins, cur, count+1, remain - coins[cur]);
+			if(helper(coins, cur, count+1, remain - coins[cur])) {
+				return true;
+			}
 			cur--;
 		}
+		return false;
 	}
     int coinChange(vector<int>& coins, int amount) {
+        if(amount == 0) return 0;
     	sort(coins.begin(), coins.end());
         found = false;
         min = INT_MAX;
-        helper(coins, coins.size() - 1, 0, amount);
-	    return found?min:-1;
+	    return (helper(coins, coins.size() - 1, 0, amount))?min:-1;
     }
 };
 
@@ -39,4 +38,8 @@ int main() {
 	vector<int> coins{1,2,5};
 	Solution s;
 	cout<<s.coinChange(coins, 11)<<endl;
+	vector<int> coins2{186,419,83,408};
+	cout<<s.coinChange(coins2, 6249)<<endl;
 }
+//[186,419,83,408]
+// 6249
